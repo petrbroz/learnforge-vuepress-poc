e93aeb39cb0803580501e9b10c2c511e366030cb
Petr Broz <ipetrbroz@gmail.com>
2021-07-27 11:19:57 +0200

    Adding custom footer.

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="footer">My custom footer</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<style scoped>
+.footer {
+  padding: 2em;
+  max-width: 740px;
+  margin: 0 auto;
+}
+</style>
+
+<script>
+import ParentLayout from "@parent-theme/layouts/Layout.vue";
+
+export default {
+  name: "Layout",
+  components: {
+    ParentLayout
+  }
+};
+</script>
